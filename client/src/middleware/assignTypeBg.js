6b08f6931e99ac364a628b619708e09f1d90027e
Your Name <you@example.com>
2020-04-27 14:32:59 -0400

    Replaced array with hash

--- client/src/middleware/assignTypeBg.js
+++ client/src/middleware/assignTypeBg.js
@@ -1,20 +1,35 @@
+// function getBg(tp){
+//     const typelist=[
+//         {type:"electric",color:"warning"},
+//         {type:"water",color:"primary"},
+//         {type:"fire",color:"danger"}
+//     ]
+
+//     let color = "secondary"
+ 
+//     typelist.forEach(item=>{
+//         if(item.type.indexOf(tp) !== -1){
+//             color = item.color
+//         }
+//     })
+
+//     return color
+ 
+// }
+
 function getBg(tp){
-    const typelist=[
-        {type:"electric",color:"warning"},
-        {type:"water",color:"primary"},
-        {type:"fire",color:"danger"}
-    ]
+    const typelist = {}
+    typelist['electric'] = 'warning'
+    typelist['water'] = 'primary'
+    typelist['fire'] = 'danger'
+    typelist['grass'] = 'success'
+    typelist['flying'] = 'dark'
 
     let color = "secondary"
- 
-    typelist.forEach(item=>{
-        if(item.type.indexOf(tp) !== -1){
-            color = item.color
-        }
-    })
 
-    return color
- 
+    if(typelist[tp]) color=typelist[tp]
+
+     return color
 }
 
 export {getBg}
